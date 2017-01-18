e5c103ba0639e99146528f6ebae55b8cfe6d1d24
Cody Brubaker <cody.brubaker@gmail.com>
2017-01-18 13:15:08 -0800

    clone skeleton

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <div class="container">
+    <h1>My first angular 2 app</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
